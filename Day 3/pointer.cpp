df2b7c10e20d0e0615a2f046405bb6112e158af4
MagiMagesh007 <112234631+MagiMagesh007@users.noreply.github.com>
2022-09-28 11:29:11 +0530

    Add files via upload

---
+++ Day 3/pointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+    int *ptrA = &a;
+    int *ptrB = &b;
+
+    cout << "Please enter the value of a : ";
+    cin >> a;
+
+    cout << "Please enter the value of b : ";
+    cin >> b;
+
+    cout << "The value of (a,b) is (" << a << "," << b << ")" << endl;
+    cout << "The pointers of (a,b) is (" << ptrA << "," << ptrB << ")" << endl;
+
+    return 0;
+}
